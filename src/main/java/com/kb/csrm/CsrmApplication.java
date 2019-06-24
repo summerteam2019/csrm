9f9be7bbf896b260b1f2925980bc28cafbdd7857
wangyi <yi.wang11@hand-china.com>
2019-06-24 20:46:12 +0800

    项目初始化

---
+++ src/main/java/com/kb/csrm/CsrmApplication.java
@@ -0,0 +1,13 @@
+package com.kb.csrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsrmApplication.class, args);
+    }
+
+}
