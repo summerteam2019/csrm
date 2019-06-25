542be27c37850d7c45065c94f9c9fbb77d2cd2f6
wangyi <yi.wang11@hand-china.com>
2019-06-25 18:58:11 +0800

    log4j设置 前端框架导入

--- src/main/java/com/kb/csrm/CsrmApplication.java
+++ src/main/java/com/kb/csrm/CsrmApplication.java
@@ -3,9 +3,20 @@ package com.kb.csrm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 系统启动类
+ *
+ * @author yiwang
+ * @version 1.0
+ * @date Created on 2019/06/25
+ */
 @SpringBootApplication
 public class CsrmApplication {
 
+    /**
+     * 系统启动方法
+     * @param args 系统参数
+     */
     public static void main(String[] args) {
         SpringApplication.run(CsrmApplication.class, args);
     }
--- src/main/java/com/kb/csrm/ServletInitializer.java
+++ src/main/java/com/kb/csrm/ServletInitializer.java
@@ -3,6 +3,11 @@ package com.kb.csrm;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author yiwang
+ * @version 1.0
+ * @date Created on 2019/06/25
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
