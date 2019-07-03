d1b3ad25057e19decd0ad9c264fe38a0483421fb
wangyi <yi.wang11@hand-china.com>
2019-07-03 18:54:38 +0800

    后台框架修复2

--- src/main/java/com/kb/csrm/CsrmApplication.java
+++ src/main/java/com/kb/csrm/CsrmApplication.java
@@ -1,5 +1,6 @@
 package com.kb.csrm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  * @date Created on 2019/06/25
  */
+@MapperScan("com.kb.csrm.user.mapper")
 @SpringBootApplication
 public class CsrmApplication {
 
